c20ee6dc96d0c123a0ccf8f720162931aed102a5
Artemka2211 <poxitivik@yandex.ru>
2025-06-24 21:24:34 +0500

    fff

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import './script';
+import './button';
+import './swiper-two';
+import './button-two';
+import './swiper3';
+import './burger';
+import './feedback';
+import './modal';
+import '../scss/style.scss';
+
+
+
+
